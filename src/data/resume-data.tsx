13c02adcfe8f31daa404ed67d50a35973abbd922
IanHobbs <ian@ianhobbs.net>
2026-06-11 14:10:57 +1000

    updating to vercel

---
+++ src/data/resume-data.tsx
@@ -0,0 +1,483 @@
+import type { ResumeData } from "@/lib/types";
+
+export const RESUME_DATA: ResumeData = {
+  name: "Ian Hobbs",
+  initials: "IJ",
+  location: "Sydney, Australia, EST",
+  locationLink: "https://www.google.com/maps/place/Sydney",
+  about:
+    "Photographer | Coder | Creative Technologist",
+  summary:
+    <>Ian Hobbs ia a specialist photographer, coder and creative technologist. He creates engaging media experiences for arts and business communities. Comfortable and highly accopmlished as a photographer who also enjoys publishing and media strategy. Work experience include lecturing and producing media content. A variety of domain independent ventures have been set up to deliver services and creative outomes. 
+    Hobbies include sailing and music composition.</>,
+  avatarUrl: "https://avatars.githubusercontent.com/ianhobbs",
+  personalWebsiteUrl: "https://ianhobbsmedia.com.au",
+  contact: {
+    email: "admin@ianhobbsmedia.com.au",
+    tel: "+61411032601",
+    social: [
+      {
+        name: "GitHub",
+        url: "https://github.com/ianhobbs",
+        icon: "github"
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/ianhobbs/",
+        icon: "linkedin",
+      },
+      {
+        name: "Instagram", 
+        url: "https://www.instagram.com/ianhobbs_media/",
+        icon: "globe",
+      },
+    ],
+  },
+  education: [
+    {
+      school: "University of Sydney, Sydney College of Art",
+      degree: "BA(Hons) Major in Photomedia / Electronic Media",
+      start: "1998",
+      end: "1999",
+    },
+    {
+      school: "University of NSW",
+      degree: "Bachelor's Degree in Fine Art, Major in Photomedia",
+      start: "1980",
+      end: "1985",
+    },
+  ],
+  work: [
+    {
+      company: "Ian Hobbs Media",
+      link: "https://ianhobbsmedia.com.au",
+      badges: ["current"],
+      title: "Media",
+      start: "2000",
+      end: "now",
+      description:
+        <>Experience includes; Videographer, Editor, Photographer, Front-end development, Animator, Design, Web production, Server and Data Management. Client and Project Management. Art Direction, Concept development. Media strategy and Editorial strategy.</>,
+    },
+    {
+      company: "University of Sydney. Sydney College of the Arts",
+      link: "https://sydney.edu.au",
+      badges: ["Sessional"],
+      title: "Tutor - Film Media",
+      start: "02/2026",
+      end: "06/2026",
+      description:
+        <>Tutoring in Film Sound design</>,
+    },
+    {
+      company: "Museum and Art Gallery of Nothern Territory",
+      link: "https://magnt.net.au",
+      badges: ["Fulltime"],
+      title: "Digitisation Officer",
+      start: "04/2024",
+      end: "10/2024",
+      description:
+        <>Digitising Collections and managing data assets. Docuementation of collection works with Fuji GFX100 and PhaseOne cameras. Digitising to FADGI-4 standards using a varueity of ICC managamant profiles. File archive and managment with Neofinder and Bash scripting. Capture-One-Pro Photoshop and other image management applications.</>,
+    },
+    {
+      company: "Codey",
+      link: "https://codey.com.au",
+      badges: ["current"],
+      title: "Web",
+      start: "2011",
+      end: "now",
+      description:
+        "Server and site maintenance. Server management and Data management. Client service. Backup management. Site optimisation. Threat mitigation",
+    },
+    {
+      company: "Sydney University SCA",
+      link: "https://www.sydney.edu.au/arts/schools/sydney-college-of-the-arts.html",
+      badges: ["Contract"],
+      title: "Research Officer",
+      start: "10/2018",
+      end: "12/2018",
+      description:
+        "Contract to research, collect and promote staff research outputs for University pages. Videography, Photography and research development. Prototyping web pages",
+    },
+    {
+      company: "Sydney University SCA",
+      link: "https://www.sydney.edu.au/arts/schools/sydney-college-of-the-arts.html",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      start: "2010",
+      end: "2014",
+      description: "Design and deliver subjects in Photography and Electronic & New Media.",
+    },
+    {
+      company: "Insearch UTS",
+      link: "https://utscollege.edu.au",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      start: "2010",
+      end: "2014",
+      description: "Deliver subjects in Digital Literacy",
+    },
+    {
+      company: "SAE + Quamtum",
+      link: "https://sae.edu.au/",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      start: "2007",
+      end: "2010",
+      description: "Deliver courses in Web, Design, Video and Animation. Audio",
+    },
+    {
+      company: "University of Newcastle. Fine Arts Faculty",
+      link: "https://www.newcastle.edu.au",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      start: "2001",
+      end: "2006",
+      description: "Design and deliver courses in Digital Imaging, Video",
+    },
+    {
+      company: "University of Western Sydney. Communications & Design.",
+      link: "https://www.westernsydney.edu.au",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      start: "2001",
+      end: "2006",
+      description: "Design and New Media, Web and Time based media.",
+    },
+    {
+      company: "Enmore TAFE",
+      link: "https://www.tafensw.edu.au/design-centre-enmore",
+      badges: ["Sessional"],
+      title: "Teacher",
+      start: "2001",
+      end: "2006",
+      description: "Design and deliver courses in New media, Flash, Web Photo-media, Design. Animation",
+    },
+    {
+      company: "Doubleday Australia Pty.Ltd",
+      link: "https://www.doubleday.com.au/",
+      badges: ["Fulltime"],
+      title: "Photographer",
+      start: "1996",
+      end: "1997",
+      description: "Producing studio product photography for direct marketing. Maintaining accounts and equipment",
+    },
+    {
+      company: "Computer Images, Crows Nest NSW",
+      link: " ",
+      badges: ["Fulltime"],
+      title: "Camera Operator",
+      start: "1990",
+      end: "1992",
+      description: "Operate and manage small photographic studio servicing internal graphic needs; maintain budget; operate a Rostrum camera to create film dupes and matts; location, corporate, architectural, product and interior photography.",
+    },
+    {
+      company: " David Graphics Pty. Ltd Sydney",
+      link: " ",
+      badges: ["Fulltime"],
+      title: "Photographer",
+      start: "1989",
+      end: "1992",
+      description: "Operate and manage small photographic studio servicing internal graphic needs; maintain budget; product photography for retail catalogues.",
+    },
+    
+  ],
+
+
+  awards: [
+    {
+      award: "Recipient of Premiers Department Grant with Penrith Regional Gallery",
+      year: "2003",
+    },
+    {
+      award: "Recipient of New Media Grant for Interactive Works, Australia Council.",
+      year: "1999",
+    },
+    {
+      award: "The Art Directors Club, New York, NY 75th Awards, 'Merit Award for Editorial Publications' for Object Magazine, Craft Council of NSW.",
+      year: "1996",
+    },
+    {
+      award: "National Gallery of Australia Collection. 'Sea Shell Headset' Joyce Hinterding & Ian Hobbs",
+      year: "1988",
+    },
+    {
+      award: "National Gallery of Australia Collection. Poster, 'May 1st Club - Smash Capital'.",
+      year: "1983",
+    },
+
+
+  ],
+
+  published: [
+    {
+      title: "Chester Nealie Etched in Fire",
+      pub: "Ron Sang Publications NZ",
+      year: "2016",
+    },
+    {
+      title: "Robyn Backen: Backspace",
+      pub: "Boccolate Publishing AU",
+      year: "2011",
+    },
+    {
+      title: "Liz Williamson: Textiles",
+      pub: "Craft Council of NSW AU",
+      year: "2009",
+    },
+    {
+      title: "For Mathew and Others: Journey with Schizophrenia",
+      pub: "The University of New South Wales AU",
+      year: "2006",
+    },
+    {
+      title: "Degree Show 1998 SCA The University of Sydney: Catalogue",
+      pub: "Sydney College of The Arts AU",
+      year: "1998",
+    },
+  ],
+
+
+  // end insert
+  
+  skills: [
+    "Photography",
+    "Studio Photography",
+    "Product Photography",
+    "Video Production",
+    "Audio Production",
+    "Project Management",
+    "Web Production",
+    "Animation",
+    "After Effects",
+    "FADGI colour control",
+    "ICC profile generation: Delta 1.4",
+    "Capture One Pro",
+    "Photoshop",
+    "SEO",
+    "Full Stack Development",
+    "E-comm management",
+    "Bash scripting",
+    "KirbyCMS",
+  ],
+  projects: [
+        {
+      title: "Arttrail Website",
+      techStack: [
+        "KirbyCMS",
+        "Tailwind",
+        "AlpineJS",
+        "Design",
+        "Full Stack Developer",
+      ],
+      description: "Created a monthly festival event website for Arttrail in Bundeena. The site features a custom calendar and event management system, as well as a dynamic gallery showcasing artists and venues.",
+      link: {
+        label: "arttrail.com.au",
+        href: "https://www.arttrail.com.au/",
+      },
+    },
+    {
+      title: "Clear Control Website",
+      techStack: [
+        "KirbyCMS",
+        "Tailwind",
+        "AlpineJS",
+        "GSAP",
+        "Design",
+        "Content",
+        "Full Stack Developer",
+      ],
+      description: "Created a business website with seductive animations and strong case study representations",
+      link: {
+        label: "clearcontrol.com.au",
+        href: "https://www.clearcontrol.com.au/",
+      },
+    },
+    {
+      title: "Cementa Website",
+      techStack: ["PerchCMS", "Web Production","Content","Photography"],
+      description:
+        "A festival site showcasing artists and Festival events",
+      link: {
+        label: "cementa.com.au",
+        href: "https://cementa.com.au/",
+      },
+    },
+    {
+      title: "A Thousand Words Website",
+      techStack: ["Web Production","Animation"],
+      description:
+        "A website of the Museum of Sydney 'A Thousand Words' exhibition",
+      link: {
+        label: "atwonline.com.au",
+        href: "https://atwonline.com.au",
+      },
+    },
+    {
+      title: "Uno",
+      techStack: ["After Effects","Video"],
+      description:
+        "Develop animations for custom Architectural LED screens for Robyn Backen",
+      link: {
+        label: "ianhobbsmedia.com.au/uno",
+        href: "https://ianhobbsmedia.com.au/uno",
+      },
+    },
+    {
+      title: "Northwest Metro Rail-link",
+      techStack: ["Photography"],
+      description:
+        "Photography of the North West Rail link Metro project",
+      link: {
+        label: "ianhobbsmedia.com.au/northwest",
+        href: "https://ianhobbsmedia.com.au/northwest-metro-architectural-photography",
+      },
+    },
+    {
+      title: "Thought Train Animation",
+      techStack: ["Video", "Website"],
+      description:
+        "Animation for Wynscreen Video project with Robyn Backen. After Effects motion graphics",
+      link: {
+        label: "ianhobbsmedia.com.au/thought-train",
+        href: "https://ianhobbsmedia.com.au/thought-train",
+      },
+    },
+    {
+      title: "Art Photography",
+      techStack: ["Art Reproduction, Hasselblad, Colour management"],
+      description: "Fine Art documentation projects. Documentation and production using Hasselblad X1d camera.",
+      link: {
+        label: "ianhobbsmedia.com.au/art",
+        href: "https://ianhobbsmedia.com.au/art",
+      },
+    },
+    {
+      title: "Research on Cultural and Religious Diversity",
+      techStack: [
+        "Photographer",
+      ],
+      description:
+        "Photographer for Dijana Alic's research research UNSW",
+      link: {
+        label: "ianhobbsmedia.com.au",
+        href: "https://ianhobbsmedia.com.au/architecture",
+      },
+    },
+    {
+      title: "Australian Ceramics Website",
+      techStack: [
+        "Full Stack Developer",
+        "PerchCMS",
+        "AlpineJS",
+        "HTMLX",
+      ],
+      description:
+        "An e-commerce site for Australian Ceramics Organisation",
+      link: {
+        label: "australianceramics.com",
+        href: "https://australianceramics.com",
+      },
+    },
+    {
+      title: "Rosie Boylan Website",
+      techStack: [
+        "Full Stack Developer",
+        "Wordpress",
+        "Shopify",
+        "liquid",
+        "Product Manager"
+      ],
+      description:
+        "An e-commerce site and shopify store for Rosie Boylan Headwear",
+      link: {
+        label: "rosieboylan.com",
+        href: "https://rosieboylan.com",
+      },
+    },
+    {
+      title: "Sue Pedley Site",
+      techStack: ["KirbyCMS", "Front End Dev", "AlpineJS"],
+      description:
+        "Portfolio site for Sue Pedley",
+      link: {
+        label: "suepedley.com.au",
+        href: "https://suepedley.com.au/",
+      },
+    },
+    {
+      title: "Turpin Crawford Studios Site",
+      techStack: ["Web development", "Photography", "Video","PerchCMS"],
+      description:
+        "Portfolio web development for some lovely artists. Lots of Video and photo.",
+      link: {
+        label: "turpincrawfordstudio.com.au/",
+        href: "https://turpincrawfordstudio.com.au/",
+      },
+    },
+    {
+      title: "Skin 8",
+      techStack: ["Web Development", "Static HTML","Pug","Sass"],
+      description:
+        "A nice high end fashion brand site by Lucy Walker",
+      link: {
+        label: "ianhobbsmedia.com.au/skin8",
+        href: "https://ianhobbsmedia.com.au/skin8",
+      },
+    },
+    {
+      title: "A Thousand Words Video",
+      techStack: ["After Effects"],
+      description:
+        "Exibiton animation of word clouds",
+      link: {
+        label: "ianhobbsmedia.com.au/a-thousand-words",
+        href: "https://ianhobbsmedia.com.au/a-thousand-words",
+      },
+    },
+    {
+      title: "Nano-black",
+      techStack: [
+        "Browser Plugin",
+        "HTML hack",
+      ],
+      description:
+        "Makes every picture black",
+      link: {
+        label: "github.com/ianhobbs/nano_black",
+        href: "https://github.com/ianhobbs/nano_black",
+      },
+    },
+    {
+      title: "Rejmyre Art Lab Site",
+      techStack: ["Web Development","PerchCMS"],
+      description:
+        "A Swedish art org's home site wonderful research.",
+      link: {
+        label: "rejmyreartlab.org/",
+        href: "https://www.rejmyreartlab.org/",
+      },
+    },
+    {
+      title: "Architectural Photography",
+      techStack: ["Photography"],
+      description:
+        "Architectural work",   
+      link: {
+        label: "ianhobbsmedia.com.au/architecture",
+        href: "https://ianhobbsmedia.com.au/architecture",
+      },
+    },
+
+    {
+      title: "Video Animation",
+      techStack: ["Video"],
+      description:
+        "Video Work",
+      
+      link: {
+        label: "ianhobbsmedia.com.au/video-animation",
+        href: "https://ianhobbsmedia.com.au/video-animation",
+      },
+    },
+  ],
+} as const;
